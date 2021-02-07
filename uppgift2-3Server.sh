e9a0f3db09d9441c80fa64dab01be20261c89cea
cindy <cindygao7809@gmail.com>
2021-02-07 13:08:59 +0100

    script for server

---
+++ uppgift2-3Server.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Nu har du loggat in remote server via ssh. Clone mitt git repository och ladda ner scripter
+git clone https://github.com/XinGclover/AWSUppgift.git
+
+# Gå in katalogen
+cd AWSUppgift
+
+# Köra scripter av uppgifter2
+./uppgift2-1.sh
+
+./uppgift2-2.sh
